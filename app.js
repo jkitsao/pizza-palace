8923e255dc0c77360f4b2787124e28a52a36c190
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-08-03 01:16:31 +0300

    add toppings selection

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const btn = document.getElementById("checkout");
 //select value of pizza size
 var s = document.getElementById("pizza-size");
 var t = document.getElementById("pizza-crust");
+var tp = document.getElementById("pizza-top");
 const deliver = document.getElementById("defaultContactFormCopy");
+//checkout modal
+const pizzaSize = document.getElementById("pizza-size");
+const pizzaPrice = document.getElementById("pizza-price");
+
 //select value of
 var price = document.getElementById("price");
 var smallPizza = 1000;
@@ -25,35 +30,79 @@ const largeCrust = {
   gluten: 740,
 };
 btn.addEventListener("click", () => {
-  //   console.log(crustType.crispy);
+  //get the modal data
+  const finalcrust = document.getElementById("finalcrust");
+  const finalsize = document.getElementById("finalsize");
+  const finalprice = document.getElementById("finalprice");
   var size = s.options[s.selectedIndex].value;
   var crust = t.options[t.selectedIndex].value;
   if (size == "small") {
-    if (crust == "crispy")
-      return (price.innerText = smallPizza + smallCrust.crispy);
-    if (crust == "stuffed")
-      return (price.innerText = smallPizza + smallCrust.stuffed);
-    if (crust == "gluten")
-      return (price.innerText = smallPizza + smallCrust.gluten);
+    if (crust == "crispy") {
+      price.innerText = smallPizza + smallCrust.crispy;
+      finalcrust.innerText = crust;
+      finalsize.innerText = size;
+      finalprice.innerText = smallPizza + smallCrust.crispy;
+    }
+    if (crust == "stuffed") {
+      price.innerText = smallPizza + smallCrust.stuffed;
+      finalcrust.innerText = crust;
+      finalsize.innerText = size;
+      finalprice.innerText = smallPizza + smallCrust.stuffed;
+    }
+
+    if (crust == "gluten") {
+      price.innerText = smallPizza + smallCrust.gluten;
+      finalcrust.innerText = crust;
+      finalsize.innerText = size;
+      finalprice.innerText = smallPizza + smallCrust.gluten;
+    }
   }
   if (size == "medium") {
-    if (crust == "crispy")
-      return (price.innerText = mediumPizza + mediumCrust.crispy);
-    if (crust == "stuffed")
-      return (price.innerText = mediumPizza + mediumCrust.stuffed);
-    if (crust == "gluten")
-      return (price.innerText = mediumPizza + mediumCrust.gluten);
+    if (crust == "crispy") {
+      price.innerText = mediumPizza + mediumCrust.crispy;
+      finalcrust.innerText = crust;
+      finalsize.innerText = size;
+      finalprice.innerText = mediumPizza + mediumCrust.crispy;
+    }
+
+    if (crust == "stuffed") {
+      price.innerText = mediumPizza + mediumCrust.stuffed;
+      finalcrust.innerText = crust;
+      finalsize.innerText = size;
+      finalprice.innerText = mediumPizza + mediumCrust.stuffed;
+    }
+
+    if (crust == "gluten") {
+      price.innerText = mediumPizza + mediumCrust.gluten;
+      finalcrust.innerText = crust;
+      finalsize.innerText = size;
+      finalprice.innerText = mediumPizza + mediumCrust.gluten;
+    }
   }
   if (size == "large") {
-    if (crust == "crispy")
-      return (price.innerText = largePizza + largeCrust.crispy);
-    if (crust == "stuffed")
-      return (price.innerText = largePizza + largeCrust.stuffed);
-    if (crust == "gluten")
-      return (price.innerText = largePizza + largeCrust.gluten);
-  }
-  console.log(deliver.value);
+    if (crust == "crispy") {
+      price.innerText = largePizza + largeCrust.crispy;
+      finalcrust.innerText = crust;
+      finalsize.innerText = size;
+      finalprice.innerText = largePizza + largeCrust.crispy;
+    }
 
+    if (crust == "stuffed") {
+      price.innerText = largePizza + largeCrust.stuffed;
+      finalcrust.innerText = crust;
+      finalsize.innerText = size;
+      finalprice.innerText = largePizza + largeCrust.stuffed;
+    }
+
+    if (crust == "gluten") {
+      price.innerText = largePizza + largeCrust.gluten;
+      finalcrust.innerText = crust;
+      finalsize.innerText = size;
+      finalprice.innerText = largePizza + largeCrust.gluten;
+    }
+  }
+
+  console.log("hello there");
   //   if (size == "medium") return (price.innerHTML = mediumPizza);
   //   if (size == "large") return (price.innerHTML = largePizza);
 });
