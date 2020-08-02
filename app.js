2095c69e02d2cc3f57596900e278a4d30551891e
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-08-02 20:34:19 +0300

    add javascript for selecting values

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-// $(document).ready(() => {
-//   alert("arrow function");
-// });
-// $("#dtBasicExample-1").mdbEditor({
-//   rowEditor: true,
-// });
-// $(".dataTables_length").addClass("bs-select");
+const btn = document.getElementById("checkout");
+var s = document.getElementById("pizza-size");
+var t = document.getElementById("pizza-toppings");
+var price = document.getElementById("price");
+var smallPizza = 1000;
+var mediumPizza = 2000;
+var largePizza = 3000;
+btn.addEventListener("click", () => {
+  var size = s.options[s.selectedIndex].value;
+  var toppings = t.options[t.selectedIndex].value;
+  if (size == "small") return (price.innerHTML = smallPizza);
+  if (size == "medium") return (price.innerHTML = mediumPizza);
+  if (size == "large") return (price.innerHTML = largePizza);
+});
