0fb415a55503d0cf7981a535abde2c048f2e1651
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-08-03 09:28:51 +0300

    add new image to readme file

---
+++ Readme.md
@@ -0,0 +1,36 @@
+# Pizza palace
+> Simple webapplication for a fictional restaurant that allows a customer to order different sizes of pizzas with one or more toppings. 
+# How it works
+* Choose the size of pizza I want e.g Small, Medium, Large
+* Choose what kind of crust I would like the pizza to have e.g * Crispy, Stuffed, Gluten-free
+* Choose the topping(s) I want on my pizza.
+* Place an order for my Pizza and see the total charge for it.
+
+
+![](./show.png)
+
+## Installation
+
+
+```sh
+git clone https://github.com/jkitsao/pizza-palace.git
+
+```
+
+
+## About me
+
+Jackson kitsao – [@YourTwitter](https://twitter.com/Jacksonkitsao5) –kitsaojackson22@gmail.com
+
+
+
+[https://github.com/jkitsao/pizza-palace](https://github.com/jkitsao/pizza-palace)
+
+## Contributing
+
+1. Fork it (<https://github.com/jkitsao/pizza-palace/fork>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
+
--- app.js
+++ app.js
@@ -29,6 +29,23 @@ const largeCrust = {
   stuffed: 400,
   gluten: 740,
 };
+//toppings
+const smallTops = {
+  mushroom: 100,
+  sausage: 240,
+  cheese: 340,
+};
+const mediumTops = {
+  mushroom: 150,
+  sausage: 270,
+  cheese: 460,
+};
+const largeTops = {
+  mushroom: 250,
+  sausage: 300,
+  cheese: 540,
+};
+
 btn.addEventListener("click", () => {
   //get the modal data
   const finalcrust = document.getElementById("finalcrust");
@@ -36,69 +53,79 @@ btn.addEventListener("click", () => {
   const finalprice = document.getElementById("finalprice");
   var size = s.options[s.selectedIndex].value;
   var crust = t.options[t.selectedIndex].value;
+  var toppings = tp.options[tp.selectedIndex].value;
+  console.log(toppings);
+
   if (size == "small") {
-    if (crust == "crispy") {
-      price.innerText = smallPizza + smallCrust.crispy;
+    if (crust == "crispy" && toppings == "mushroom") {
+      price.innerText = smallPizza + smallCrust.crispy + smallTops.mushroom;
       finalcrust.innerText = crust;
       finalsize.innerText = size;
-      finalprice.innerText = smallPizza + smallCrust.crispy;
+      finalprice.innerText =
+        smallPizza + smallCrust.crispy + smallTops.mushroom;
     }
-    if (crust == "stuffed") {
-      price.innerText = smallPizza + smallCrust.stuffed;
+    if (crust == "stuffed" && toppings == "sausage") {
+      price.innerText = smallPizza + smallCrust.stuffed + smallTops.sausage;
       finalcrust.innerText = crust;
       finalsize.innerText = size;
-      finalprice.innerText = smallPizza + smallCrust.stuffed;
+      finalprice.innerText =
+        smallPizza + smallCrust.stuffed + smallTops.sausage;
     }
 
-    if (crust == "gluten") {
-      price.innerText = smallPizza + smallCrust.gluten;
+    if (crust == "gluten" && toppings == "cheese") {
+      price.innerText = smallPizza + smallCrust.gluten + smallTops.cheese;
       finalcrust.innerText = crust;
       finalsize.innerText = size;
-      finalprice.innerText = smallPizza + smallCrust.gluten;
+      finalprice.innerText = smallPizza + smallCrust.gluten + smallTops.cheese;
     }
   }
   if (size == "medium") {
-    if (crust == "crispy") {
-      price.innerText = mediumPizza + mediumCrust.crispy;
+    if (crust == "crispy" && toppings == "mushroom") {
+      price.innerText = mediumPizza + mediumCrust.crispy + mediumTops.mushroom;
       finalcrust.innerText = crust;
       finalsize.innerText = size;
-      finalprice.innerText = mediumPizza + mediumCrust.crispy;
+      finalprice.innerText =
+        mediumPizza + mediumCrust.crispy + mediumTops.mushroom;
     }
 
-    if (crust == "stuffed") {
-      price.innerText = mediumPizza + mediumCrust.stuffed;
+    if (crust == "stuffed" || toppings == "sausage") {
+      price.innerText = mediumPizza + mediumCrust.stuffed + mediumTops.sausage;
       finalcrust.innerText = crust;
       finalsize.innerText = size;
-      finalprice.innerText = mediumPizza + mediumCrust.stuffed;
+      finalprice.innerText =
+        mediumPizza + mediumCrust.stuffed + mediumTops.sausage;
     }
 
-    if (crust == "gluten") {
-      price.innerText = mediumPizza + mediumCrust.gluten;
+    if (crust == "gluten" && toppings == "cheese") {
+      price.innerText = mediumPizza + mediumCrust.gluten + mediumTops.cheese;
       finalcrust.innerText = crust;
       finalsize.innerText = size;
-      finalprice.innerText = mediumPizza + mediumCrust.gluten;
+      finalprice.innerText =
+        mediumPizza + mediumCrust.gluten + mediumTops.cheese;
     }
   }
   if (size == "large") {
-    if (crust == "crispy") {
-      price.innerText = largePizza + largeCrust.crispy;
+    if (crust == "crispy" && toppings == "mushroom") {
+      price.innerText = largePizza + largeCrust.crispy + largeTops.mushroom;
       finalcrust.innerText = crust;
       finalsize.innerText = size;
-      finalprice.innerText = largePizza + largeCrust.crispy;
+      finalprice.innerText =
+        largePizza + largeCrust.crispy + largeTops.mushroom;
     }
 
-    if (crust == "stuffed") {
-      price.innerText = largePizza + largeCrust.stuffed;
+    if (crust == "stuffed" && toppings == "sausage") {
+      price.innerText = largePizza + largeCrust.stuffed + largeTops.sausage;
       finalcrust.innerText = crust;
       finalsize.innerText = size;
-      finalprice.innerText = largePizza + largeCrust.stuffed;
+      finalprice.innerText =
+        largePizza + largeCrust.stuffed + largeTops.sausage;
     }
 
-    if (crust == "gluten") {
-      price.innerText = largePizza + largeCrust.gluten;
+    if (crust == "gluten" && toppings == "cheese") {
+      price.innerText = largePizza + largeCrust.gluten + largeTops.cheese;
       finalcrust.innerText = crust;
       finalsize.innerText = size;
-      finalprice.innerText = largePizza + largeCrust.gluten;
+      finalprice.innerText = largePizza + largeCrust.gluten + largeTops.cheese;
     }
   }
 
